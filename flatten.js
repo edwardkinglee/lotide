3787f45dbd89dcbe80004e03c9843078ffa9abbe
edwardkinglee <edward.lee3@telus.com>
2022-11-21 17:21:25 -0500

    removed tests from flatten.js and added export

--- flatten.js
+++ flatten.js
@@ -1,15 +1,3 @@
-const eqArrays = (a, b) =>
-  a.length === b.length &&
-  a.every((v, i) => v === b[i]);
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`Arrays are equal \u2705: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Arrays are not equal \u274C: [${array1}] !== [${array2}]`);
-  }
-};
-
 const flatten = (array) => {
   const newArray = [];
 
@@ -25,4 +13,4 @@ const flatten = (array) => {
   return newArray;
 };
 
-assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+module.exports = flatten;
