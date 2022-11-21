4750e7fbf7428374b82ed9cf1125e3250cd9d8a6
edwardkinglee <edward.lee3@telus.com>
2022-11-21 17:23:20 -0500

    added export to letterPositions.js

--- letterPositions.js
+++ letterPositions.js
@@ -1,15 +1,3 @@
-const eqArrays = (a, b) =>
-  a.length === b.length &&
-  a.every((v, i) => v === b[i]);
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`Arrays are equal \u2705: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Arrays are not equal \u274C: [${array1}] !== [${array2}]`);
-  }
-};
-  
 const letterPositions = function(sentence) {
   const results = {};
   const sentenceArray = sentence.split('');
@@ -27,11 +15,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-const result1 = letterPositions('hello');
-
-//Test Code
-assertArraysEqual(result1['h'], [0]);
-assertArraysEqual(result1['e'], [1]);
-assertArraysEqual(result1['l'], [2,3]);
-assertArraysEqual(result1['o'], [4]);
-
+module.exports = letterPositions;
