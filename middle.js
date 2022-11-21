3a3999fa877dcaead3acca0b526fa2b41c0a182a
edwardkinglee <edward.lee3@telus.com>
2022-11-21 03:33:54 -0500

    added Mocha and Chai, updated test for headTest.js

--- middle.js
+++ middle.js
@@ -1,15 +1,3 @@
-const eqArrays = (a, b) =>
-  a.length === b.length &&
-  a.every((v, i) => v === b[i]);
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`Arrays are equal \u2705: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Arrays are not equal \u274C: [${array1}] !== [${array2}]`);
-  }
-};
-
 const middle = (array) => {
   const middleIndex = Math.floor(array.length / 2);
 
@@ -23,11 +11,4 @@ const middle = (array) => {
   
 };
 
-assertArraysEqual(middle([1]), []); // => []
-assertArraysEqual(middle([1, 2]), []); // => []
-
-assertArraysEqual(middle([1, 2, 3]), [2]); // => [2]
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]); // => [3]
-
-assertArraysEqual(middle([1, 2, 3, 4]), [2,3]); // => [2, 3]
-assertArraysEqual(middle([1, 2, 3, 4, 5, 6]), [3,4]); // => [3, 4]
+module.exports = middle;
