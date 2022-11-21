afdf907d2e9706bd558bdbcb03482a9511d1e8c6
edwardkinglee <edward.lee3@telus.com>
2022-11-21 16:54:17 -0500

    removed tests from countLetters and created test file

--- countLetters.js
+++ countLetters.js
@@ -1,13 +1,3 @@
-// Function Implementation
-
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed \u2705: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed \u274C: ${actual} !== ${expected}`);
-  }
-};
-
 const countLetters = (sentence) => {
   const letterCount = {};
 
@@ -22,19 +12,4 @@ const countLetters = (sentence) => {
   return letterCount;
 };
 
-const result1 = countLetters('LHL');
-const result2 = countLetters("lighthouse in the house");
-//assertEqual for result1
-assertEqual(result1['L'], 2);
-assertEqual(result1['H'], 1);
-//assertEqual for result2
-assertEqual(result2['l'], 1);
-assertEqual(result2['i'], 2);
-assertEqual(result2['g'], 1);
-assertEqual(result2['h'], 4);
-assertEqual(result2['t'], 2);
-assertEqual(result2['o'], 2);
-assertEqual(result2['u'], 2);
-assertEqual(result2['s'], 2);
-assertEqual(result2['e'], 3);
-assertEqual(result2['n'], 1);
+module.exports = countLetters;
