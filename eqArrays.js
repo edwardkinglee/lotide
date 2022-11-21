86356106f66c97173f632b0c030fb28f804b1299
edwardkinglee <edward.lee3@telus.com>
2022-11-21 02:37:44 -0500

    added eqArraysTest file with tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,11 +1,3 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed \u2705: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed \u274C: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = (array1, array2) => {
 
   if (array1.length !== array2.length) {
@@ -24,6 +16,5 @@ const eqArrays = (array1, array2) => {
   return true;
 };
 
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true); // => true
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false); // => false
-assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false); // => false
+module.exports = eqArrays;
+
