fa58d2619dba6fcb99c7fbafd4d84564eed9eecd
edwardkinglee <edward.lee3@telus.com>
2022-11-21 17:35:23 -0500

    removed test case from takeUntil.js and added export

--- takeUntil.js
+++ takeUntil.js
@@ -1,15 +1,3 @@
-const eqArrays = (a, b) =>
-  a.length === b.length &&
-  a.every((v, i) => v === b[i]);
-
-const assertArraysEqual = (array1, array2) => {
-  if (eqArrays(array1, array2)) {
-    console.log(`Arrays are equal \u2705: [${array1}] === [${array2}]`);
-  } else {
-    console.log(`Arrays are not equal \u274C: [${array1}] !== [${array2}]`);
-  }
-};
-  
 const takeUntil = function(array, callback) {
   const newArray = [];
   
@@ -22,17 +10,4 @@ const takeUntil = function(array, callback) {
   return newArray;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
-
-/*Expected Output
-[ 1, 2, 5, 7, 2 ]
---
-[ 'I\'ve', 'been', 'to', 'Hollywood' ]*/
+module.exports = takeUntil;
