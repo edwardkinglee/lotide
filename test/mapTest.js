d6e4467af9f4696c1b9a3c8e9e30b76988fe1573
edwardkinglee <edward.lee3@telus.com>
2022-11-21 23:00:31 -0500

    updated tests

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,15 +1,35 @@
+const assert = require('chai').assert;
 const index = require('../index');
 
-const words = ["ground", "control", "to", "major", "tom"];
+describe("#map", () => {
+  it("returns [ 'g', 'c', 't', 'm', 't' ] for ['ground', 'control', 'to', 'major', 'tom'], function(item) { return item[0];})", () => {
 
-index.assertArraysEqual(index.map(words, function(item) {
-  return item[0];
-}),[ 'g', 'c', 't', 'm', 't' ]);
+    const words = ["ground", "control", "to", "major", "tom"];
 
-index.assertArraysEqual(index.map(words, function(item) {
-  return item.slice(1);
-}),[ 'round', 'ontrol', 'o', 'ajor', 'om' ]);
+    assert.deepEqual(index.map(words, function(item) {
+      return item[0];
+    }),
+    [ 'g', 'c', 't', 'm', 't' ]);
+  });
 
-index.assertArraysEqual(index.map(words, function(item) {
-  return item + 's';
-}),[ 'grounds', 'controls', 'tos', 'majors', 'toms' ]);
+  it("[ 'round', 'ontrol', 'o', 'ajor', 'om' ] for ['ground', 'control', 'to', 'major', 'tom'], function(item) { return item.slice(1);})", () => {
+
+    const words = ["ground", "control", "to", "major", "tom"];
+
+    assert.deepEqual(index.map(words, function(item) {
+      return item.slice(1);
+    }),
+    [ 'round', 'ontrol', 'o', 'ajor', 'om' ]);
+  });
+
+  it("returns ['grounds', 'controls', 'tos', 'majors', 'toms' ] for ['ground', 'control', 'to', 'major', 'tom'], function(item) { return item + 's';})", () => {
+
+    const words = ["ground", "control", "to", "major", "tom"];
+
+    assert.deepEqual(index.map(words, function(item) {
+      return item + 's';
+    }),
+    ['grounds', 'controls', 'tos', 'majors', 'toms' ]);
+  });
+
+});
--- test/takeUntilTest.js
+++ test/takeUntilTest.js
@@ -1,16 +1,16 @@
+const assert = require('chai').assert;
 const index = require('../index');
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = index.takeUntil(data1, x => x < 0);
-index.assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+describe("#takeUntilTest", () => {
 
-console.log('---');
+  it("returns [ 1, 2, 5, 7, 2 ] for [1, 2, 5, 7, 2, -1, 2, 4, 5], x => x < 0)", () => {
+    const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+    assert.deepEqual(index.takeUntil(data1, x => x < 0), [ 1, 2, 5, 7, 2 ]);
+  });
 
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = index.takeUntil(data2, x => x === ',');
-index.assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+  it("returns [ 'I've', 'been', 'to', 'Hollywood' ] for (['I've', 'been', 'to', 'Hollywood', ',', 'I've', 'been', 'to', 'Redwood'], x => x === ',')", () => {
+    const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+    assert.deepEqual(index.takeUntil(data2, x => x === ','), [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+  });
 
-/*Expected Output
-[ 1, 2, 5, 7, 2 ]
---
-[ 'I\'ve', 'been', 'to', 'Hollywood' ]*/
+});
