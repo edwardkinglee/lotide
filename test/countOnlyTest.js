ec6972a722246a172dec63e6e2ee717974b8a3f4
edwardkinglee <edward.lee3@telus.com>
2022-11-21 21:12:47 -0500

    updated test files added assertions

--- test/countOnlyTest.js
+++ test/countOnlyTest.js
@@ -1,20 +1,50 @@
+const assert = require('chai').assert;
 const index = require('../index');
 
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
+describe("#countOnly", () => {
+  it("returns {Fang: 2, Jason: 1} for (['Karl', 'Salima', 'Agouhanna', 'Fang','Kavith', 'Jason', 'Salima', 'Fang', 'Joe'], { 'Jason': true, 'Karima': true, 'Fang': true, 'Agouhanna': false })", () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.deepEqual(index.countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }), { Fang: 2, Jason: 1 });
+  });
 
-const result1 = index.countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+  it("returns  Agouhanna: 1, Fang: 2 } for (['Karl', 'Salima', 'Agouhanna', 'Fang','Kavith', 'Jason', 'Salima', 'Fang', 'Joe'], { 'Jason': false, 'Karima': true, 'Fang': true, 'Agouhanna': true}))", () => {
+    const firstNames = [
+      "Karl",
+      "Salima",
+      "Agouhanna",
+      "Fang",
+      "Kavith",
+      "Jason",
+      "Salima",
+      "Fang",
+      "Joe"
+    ];
+    assert.deepEqual(index.countOnly(firstNames, { "Jason": false, "Karima": true, "Fang": true, "Agouhanna": true }), { Agouhanna: 1, Fang: 2 });
+  });
 
-index.assertEqual(result1["Jason"], 1);
-index.assertEqual(result1["Karima"], undefined);
-index.assertEqual(result1["Fang"], 2);
-index.assertEqual(result1["Agouhanna"], undefined);
+  it("returns  { Edward: 2 } for (['Karl', 'Salima', 'Agouhanna', 'Fang','Kavith', 'Edward', 'Salima', 'Fang', 'Joe', 'Edward'], { 'Jason': false, 'Karima': true, 'Fang': true, 'Agouhanna': false, 'Edward': true }))", () => {
+    const firstNames = [
+      'Karl',
+      'Salima',
+      'Agouhanna',
+      'Fang',
+      'Kavith',
+      'Edward',
+      'Salima',
+      'Fang',
+      'Joe',
+      'Edward'
+    ];
+    assert.deepEqual(index.countOnly(firstNames, { "Jason": false, "Karima": true, "Fang": false, "Agouhanna": false, 'Edward': true }), { Edward: 2 });
+  });
+});
--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,9 +1,16 @@
+const assert = require('chai').assert;
 const index = require('../index');
 
-const result1 = index.letterPositions('hello');
+describe("#letterPositions", () => {
+  it("returns { h: [0], e: [1], l: [2, 3] ,o: [4] } for 'hello'", () => {
+    assert.deepEqual(index.letterPositions('hello'), { h: [0], e: [1], l: [2, 3], o: [4] });
+  });
 
-//Test Code
-index.assertArraysEqual(result1['h'], [0]);
-index.assertArraysEqual(result1['e'], [1]);
-index.assertArraysEqual(result1['l'], [2,3]);
-index.assertArraysEqual(result1['o'], [4]);
+  it("returns { l: [ 0 ], i: [ 1 ], g: [ 2 ], h: [ 3, 5 ], t: [ 4 ], o: [ 6 ], u: [ 7 ], s: [ 8 ], e: [ 9 ] } for 'lighthouse'", () => {
+    assert.deepEqual(index.letterPositions('lighthouse'), { l: [0], i: [1], g: [2], h: [3, 5], t: [4], o: [6], u: [7], s: [8], e: [9] });
+  });
+
+  it("returns { l: [ 0 ], a: [ 1 ], b: [ 2 ], s: [ 3 ] } for 'labs'", () => {
+    assert.deepEqual(index.letterPositions('labs'), { l: [ 0 ], a: [ 1 ], b: [ 2 ], s: [ 3 ] });
+  });
+});
