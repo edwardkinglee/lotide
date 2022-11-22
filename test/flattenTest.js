8a03d542f31d2c8b8e9604766ebc3235364436c6
edwardkinglee <edward.lee3@telus.com>
2022-11-21 20:18:54 -0500

    added mocha/chai tests to flattenTest.js

--- test/flattenTest.js
+++ test/flattenTest.js
@@ -1,3 +1,20 @@
+const assert = require('chai').assert;
 const index = require('../index');
 
-index.assertArraysEqual(index.flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
+describe("#flatten", () => {
+  it("returns [1, 2, 3, 4, 5, 6] for [1, 2, [3, 4], 5, [6]]", () => {
+    assert.deepEqual(index.flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
+  });
+
+  it("returns [1, 2, 3, 4, 5, 6, 7, 8] for [1, 2, [3, 4], 5, [6], [7,8]]", () => {
+    assert.deepEqual(index.flatten([1, 2, [3, 4], 5, [6], [7,8]]), [1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it("returns [1, 2, 3, 4, 5, 6, 7, 8] for [1, 2, [3, 4, 5], [6], [7,8]]", () => {
+    assert.deepEqual(index.flatten([1, 2, [3, 4, 5], [6], [7,8]]), [1, 2, 3, 4, 5, 6, 7, 8]);
+  });
+
+  it("returns [1, 2, 3, 4, 5, 6, 7, 8, 9] for [[1, 2, 3, 4], 5, [6], [7, 8, 9]]", () => {
+    assert.deepEqual(index.flatten([[1, 2, 3, 4], 5, [6], [7, 8, 9]]), [1, 2, 3, 4, 5, 6, 7, 8, 9]);
+  });
+});
